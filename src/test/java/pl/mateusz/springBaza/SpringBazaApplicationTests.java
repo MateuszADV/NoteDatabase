e288b02a80cf0dc9060f4ee0eb6e0fda1b585930
MateuszADV <mateusz.adv@gmail.com>
2018-04-11 18:44:30 +0200

    Added details order

---
+++ src/test/java/pl/mateusz/springBaza/SpringBazaApplicationTests.java
@@ -0,0 +1,16 @@
+package pl.mateusz.springBaza;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringBazaApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
